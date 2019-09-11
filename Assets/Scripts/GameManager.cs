5cdfe5b8905e6221ee8a9a3e02a1a8dced4a52f2
joanby <joan-by@hotmail.com>
2019-09-11 19:30:12 +0200

    GUI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
 
+  
     private int numberOfEnemies = 0;
     private int score = 0;
 
+    private float totalTime = 0;
 
+
+    public bool showWinScreen = false;
 
     private int playerHP = 3;
     public int HP {
@@ -22,6 +28,9 @@ public class GameManager : MonoBehaviour
     }
 
 
+    public string labelText = "Recoge los 4 objetos y gana!";
+    public int maxItems = 4;
+
 
     private int itemsCollected = 0;
     public int Items{
@@ -29,6 +38,15 @@ public class GameManager : MonoBehaviour
         set{
             itemsCollected = value;
             Debug.LogFormat("Items: {0}", itemsCollected);
+
+
+            if(itemsCollected >= maxItems){
+                showWinScreen = true;
+                Time.timeScale = 0;
+                labelText = "Has encontrado todos los ítems";
+            }else{
+                labelText = "Item econtrado. Faltan: " + (maxItems - itemsCollected);
+            }
         }
     }
 
@@ -42,19 +60,38 @@ public class GameManager : MonoBehaviour
         set{
             _playerName = value;
         } 
-    } 
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-      
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+
+    private void Update()
     {
-        
+        totalTime += Time.deltaTime;
+    }
+
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(20, 20, 150, 50), "Player Health: " + playerHP);
+        GUI.Box(new Rect(20, 90, 150, 50), "Items collected: " + itemsCollected);
+        GUI.Box(new Rect(20, 150, 150, 50), "Record Previo: " + PlayerPrefs.GetFloat("record", 999999));
+
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 200, 50),
+                  labelText);
+
+        if(showWinScreen){
+            if(GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 100,
+                                   400, 200), "GAME OVER!\n"+totalTime)){
+                float previousRecord = PlayerPrefs.GetFloat("record", 999999);
+
+                if(previousRecord >= totalTime){
+                    PlayerPrefs.SetFloat("record", totalTime);
+                }
+
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                Time.timeScale = 1;
+            }
+        }
     }
 }
