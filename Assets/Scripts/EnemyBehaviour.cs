58216d2a454596a7b6466d3030bf66114e3001a7
joanby <joan-by@hotmail.com>
2019-09-12 17:07:46 +0200

    IA

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,10 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
+
 
 [RequireComponent(typeof(SphereCollider))]
 public class EnemyBehaviour : MonoBehaviour
 {
+
+    public Transform patrolRoute;
+
+    public List<Transform> locations;
+
+    private int currentLocation = 0;
+
+    private NavMeshAgent _agent;
+
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    void InitializePatrolRoute(){
+        foreach(Transform t in patrolRoute){
+            locations.Add(t);
+        }
+    }
+
+
+    void MoveToNextPatrolLocation(){
+
+        if(locations.Count==0){
+            return;
+        }
+
+        _agent.destination = locations[currentLocation].position;
+
+        currentLocation = (currentLocation + 1) % locations.Count;
+    }
+
+    private void Update()
+    {
+        if(_agent.remainingDistance < _agent.stoppingDistance &&
+           !_agent.pathPending){
+            MoveToNextPatrolLocation();
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
