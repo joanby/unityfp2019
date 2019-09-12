766d2db615628efe35f13ed91d2b8c5416d323df
joanby <joan-by@hotmail.com>
2019-09-12 17:41:52 +0200

    Camera Switch + Orc

---
+++ Assets/Scripts/CameraSwitching.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitching : MonoBehaviour
+{
+
+	public Camera c1, c2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c1.gameObject.SetActive(true);
+        c2.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C)){
+            c1.gameObject.SetActive(!c1.gameObject.activeInHierarchy);
+            c2.gameObject.SetActive(!c2.gameObject.activeInHierarchy);
+        }
+    }
+}
